34e948903247c81e17be1f69dad97ac758d73480
zavodil <zavodil.ru@gmail.com>
2024-08-30 08:16:05 -0400

    Create ws.js

---
+++ ws.js
@@ -0,0 +1,15 @@
+import { setupWalletSelector } from "@near-wallet-selector/core";
+import { setupModal } from "@near-wallet-selector/modal-ui-js";
+import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
+import { setupHereWallet } from "@near-wallet-selector/here-wallet";
+import { setupMeteorWallet } from "@near-wallet-selector/meteor-wallet";
+
+const selector = await setupWalletSelector({
+  network: "testnet",
+  modules: [setupMyNearWallet(), setupHereWallet(), setupMeteorWallet()],
+});
+
+const modal = setupModal(selector, {});
+
+window.selector = selector;
+window.modal = modal;
